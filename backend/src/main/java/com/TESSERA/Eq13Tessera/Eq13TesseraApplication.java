7be77a3aadde2160fb0596d0a86b32d4bdf8333c
AlondraPliego <alondrapliego131104@gmail.com>
2026-07-25 23:53:20 -0600

    Backend Base

---
+++ backend/src/main/java/com/TESSERA/Eq13Tessera/Eq13TesseraApplication.java
@@ -0,0 +1,13 @@
+package com.TESSERA.Eq13Tessera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eq13TesseraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eq13TesseraApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/TESSERA/Eq13Tessera/Eq13TesseraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TESSERA.Eq13Tessera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eq13TesseraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
